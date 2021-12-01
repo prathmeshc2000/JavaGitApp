416e6f6f862994d406a85db64957447b0d0527ce
prathmeshc2000 <chaudhariprathamesh1@gmail.com>
2021-12-01 14:25:41 +0530

    first commit

---
+++ JavaGitApp/src/main/java/com/lti/demo1/ClientApp.java
@@ -0,0 +1,11 @@
+package com.lti.demo1;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Product p1=new Product(101,"Mobile");
+		System.out.println(p1);
+	}
+
+}
---
+++ JavaGitApp/src/main/java/com/lti/demo1/Product.java
@@ -0,0 +1,37 @@
+package com.lti.demo1;
+
+public class Product {
+	private int prdId;
+	private String prdName;
+	
+	
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getPrdId() {
+		return prdId;
+	}
+	public void setPrdId(int prdId) {
+		this.prdId = prdId;
+	}
+	public String getPrdName() {
+		return prdName;
+	}
+	public void setPrdName(String prdName) {
+		this.prdName = prdName;
+	}
+
+	public Product(int prdId, String prdName) {
+		super();
+		this.prdId = prdId;
+		this.prdName = prdName;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [prdId=" + prdId + ", prdName=" + prdName + "]";
+	}
+
+}
